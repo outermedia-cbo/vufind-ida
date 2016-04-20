bb7f437c18f5ed2825a57c44ed87ec2206579769
Robert <rpr@outermedia.de>
2016-04-20 17:46:30 +0200

    debug deployment script

--- travis_deploy_dev.sh
+++ travis_deploy_dev.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 DEPLOY_PORT="2222" 
-
 # Add Server to known_hosts manually because variables are not evaluated in addons phase.
+echo path: $DEPLOY_PATH
+echo Add server to known hosts
 ssh-keyscan -t $TRAVIS_SSH_KEY_TYPES -p $DEPLOY_PORT -H $DEPLOY_HOST 2>&1 | tee -a $HOME/.ssh/known_hosts
 
 ## Fetch VuFind and build package
@@ -11,7 +12,9 @@ ssh-keyscan -t $TRAVIS_SSH_KEY_TYPES -p $DEPLOY_PORT -H $DEPLOY_HOST 2>&1 | tee 
 #cp -R themes module ida languages solr vufind-$VUFIND_VERSION/
 
 # SCP to Server
+echo Prepare key
 chmod 0600 ssh_ida_travis_rsa
 #scp -C -r -i ssh_ida_travis_rsa -P 2222 -p vufind-$VUFIND_VERSION/* $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
 #scp -C -q -r -i ssh_ida_travis_rsa -P 2222 -p themes module ida languages solr $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+chmod rsync to server 
 rsync -az -e "ssh -p $DEPLOY_PORT -i ssh_ida_travis_rsa" themes module ida languages solr $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
