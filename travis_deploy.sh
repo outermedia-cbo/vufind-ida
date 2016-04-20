7d53ddbbc5e14106eedd28a45a9f3265aaeaf188
Robert <rpr@outermedia.de>
2016-04-20 18:28:18 +0200

    Call deployment script from after_success Add .gitignore

--- travis_deploy.sh
+++ travis_deploy.sh
@@ -1,8 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
-DEPLOY_PORT="2222" 
+set -o errexit -o nounset
+
+echo "This commit was made against the $TRAVIS_BRANCH."
+if [ "$TRAVIS_BRANCH" == "stage" ]
+then
+  echo "Deploying to Stage"
+  export DEPLOY_PORT=2223
+else
+  echo "Deploying to Dev"
+  export DEPLOY_PORT=2222
+fi
+
 # Add Server to known_hosts manually because variables are not evaluated in addons phase.
-echo path: $DEPLOY_PATH
 echo Add server to known hosts
 ssh-keyscan -t $TRAVIS_SSH_KEY_TYPES -p $DEPLOY_PORT -H $DEPLOY_HOST 2>&1 | tee -a $HOME/.ssh/known_hosts
 
@@ -16,5 +26,5 @@ echo Prepare key
 chmod 0600 ssh_ida_travis_rsa
 #scp -C -r -i ssh_ida_travis_rsa -P 2222 -p vufind-$VUFIND_VERSION/* $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
 #scp -C -q -r -i ssh_ida_travis_rsa -P 2222 -p themes module ida languages solr $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
-chmod rsync to server 
+echo RSYNC to server
 rsync -az -e "ssh -p $DEPLOY_PORT -i ssh_ida_travis_rsa" themes module ida languages solr $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
