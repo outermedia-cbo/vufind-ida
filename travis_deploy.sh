03e87187cca6711d4dae86aa984f2a754136ce25
Robert <rpr@outermedia.de>
2016-04-20 18:37:31 +0200

    fix ssh-keyscan call

--- travis_deploy.sh
+++ travis_deploy.sh
@@ -2,7 +2,7 @@
 
 set -o errexit -o nounset
 
-echo "This commit was made against the $TRAVIS_BRANCH."
+echo "This commit was made against the $TRAVIS_BRANCH branch."
 if [ "$TRAVIS_BRANCH" == "stage" ]
 then
   echo "Deploying to Stage"
@@ -14,7 +14,7 @@ fi
 
 # Add Server to known_hosts manually because variables are not evaluated in addons phase.
 echo Add server to known hosts
-ssh-keyscan -t $TRAVIS_SSH_KEY_TYPES -p $DEPLOY_PORT -H $DEPLOY_HOST 2>&1 | tee -a $HOME/.ssh/known_hosts
+ssh-keyscan -p $DEPLOY_PORT -H $DEPLOY_HOST 2>&1 | tee -a $HOME/.ssh/known_hosts
 
 ## Fetch VuFind and build package
 #curl http://heanet.dl.sourceforge.net/project/vufind/VuFind/2.3.1/vufind-$VUFIND_VERSION.tar.gz > vufind-$VUFIND_VERSION.tar.gz
