a8125f6e6c463322924c8d2fa2a87140d5586f2d
Robert <rpr@outermedia.de>
2016-04-20 18:48:41 +0200

    Deactivate StrictHostKeyChecking instead of add server to known_hosts each time since there is no security against man in the middle attacks in both cases

--- travis_deploy.sh
+++ travis_deploy.sh
@@ -13,8 +13,8 @@ else
 fi
 
 # Add Server to known_hosts manually because variables are not evaluated in addons phase.
-echo Add server to known hosts
-ssh-keyscan -p $DEPLOY_PORT -H $DEPLOY_HOST 2>&1 | tee -a $HOME/.ssh/known_hosts
+#echo Add server to known hosts
+#ssh-keyscan -p $DEPLOY_PORT -H $DEPLOY_HOST 2>&1 | tee -a $HOME/.ssh/known_hosts
 
 ## Fetch VuFind and build package
 #curl http://heanet.dl.sourceforge.net/project/vufind/VuFind/2.3.1/vufind-$VUFIND_VERSION.tar.gz > vufind-$VUFIND_VERSION.tar.gz
@@ -27,4 +27,4 @@ chmod 0600 ssh_ida_travis_rsa
 #scp -C -r -i ssh_ida_travis_rsa -P 2222 -p vufind-$VUFIND_VERSION/* $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
 #scp -C -q -r -i ssh_ida_travis_rsa -P 2222 -p themes module ida languages solr $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
 echo RSYNC to server
-rsync -az -e "ssh -p $DEPLOY_PORT -i ssh_ida_travis_rsa" themes module ida languages solr $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+rsync -az -e "ssh -o StrictHostKeyChecking=no -o LogLevel=ERROR -p $DEPLOY_PORT -i ssh_ida_travis_rsa" themes module ida languages solr $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
